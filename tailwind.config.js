3a29410205a56857f1e73a4fd45a65e1800f9875
MintarasGrinius <74651345+MintarasGrinius@users.noreply.github.com>
2022-12-10 11:19:07 +0200

    Menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
